93413d2b70b840f24e7b82df692354b1da4f1e38
Warmcloud1 <52983230+Warmcloud1@users.noreply.github.com>
2022-04-13 12:12:14 +0800

    Create noun.java

---
+++ Kotlin/noun.java
@@ -0,0 +1,51 @@
+val age = 5
+val name = "Rover"
+//val is a special word used by Kotlin, called a keyword, indicating that what follows is the name of a variable.
+  
+//age is the name of the variable.
+  
+//= makes the value of age (on its left) be the same as the value on its right. In math, a single equal sign is used to assert that the values on each side are the same.
+//   In Kotlin, unlike in math, a single equal sign is used to assign the value on the right to the named variable on the left.
+  
+  println("You are already ${age}! ${name}")
+  
+  printBorder()
+  printBorder2()
+  printBorder3("=",18)   
+  
+  
+//learn how to simplify that code using techniques for repeating and reusing code,
+  Case1:一般
+fun printBorder(){
+    println("==================")
+}
+
+  Case2:使用repeat
+fun printBorder2(){
+    repeat(18){
+        print("=")
+    }
+    println()
+}
+
+  Case3:增加參數
+fun printBorder3(border:String,timeToRepeat:Int){  //parameter
+    repeat(timeToRepeat){
+        print(border)
+    }
+    println()
+}
+
+  Case4:回傳數字
+fun sum(a: Int, b: Int): Int {
+    return a + b
+}
+
+  Case5:returns no meaningful value.
+fun printSum(a: Int, b: Int): Unit { //:Unit 可以省略
+    println("sum of $a and $b is ${a + b}")
+}
+
+Case6:A function body can be an expression. Its return type is inferred.
+  fun sum2(a:Int,b:Int)=a+b
+
