a5d5dca26cd9af34c6ec390fabedca5e2f52563b
Warmcloud1 <52983230+Warmcloud1@users.noreply.github.com>
2022-04-13 14:17:30 +0800

    Create 2_4WhileLoop.kt

---
+++ Kotlin/2_4WhileLoop.kt
@@ -0,0 +1,12 @@
+//while
+val items = listOf("apple", "banana", "kiwifruit")
+var index = 0
+while (index < items.size) {
+    println("item at $index is ${items[index]}")
+    index++
+}
+/*
+item at 0 is apple
+item at 1 is banana
+item at 2 is kiwifruit
+*/
