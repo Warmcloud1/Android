d4e0ba263e4a63ecf903fd112a811ae502c30981
Warmcloud1 <52983230+Warmcloud1@users.noreply.github.com>
2022-04-13 17:32:14 +0800

    Update 2_4WhileLoop.kt

--- Kotlin/2_4WhileLoop.kt
+++ Kotlin/2_4WhileLoop.kt
@@ -10,3 +10,9 @@ item at 0 is apple
 item at 1 is banana
 item at 2 is kiwifruit
 */
+
+//do-while executes the body and then checks the condition. If it's satisfied, the loop repeats.
+// So, the body of do-while executes at least once regardless of the condition.
+do {
+    val y = retrieveData()
+} while (y != null) // y is visible here!
