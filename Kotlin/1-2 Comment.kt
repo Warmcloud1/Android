148ec25dd7b80ecb26086733f3b9a9796e570187
Warmcloud1 <52983230+Warmcloud1@users.noreply.github.com>
2022-04-13 14:03:44 +0800

    Create 1-2 Comment.kt

---
+++ Kotlin/1-2 Comment.kt
@@ -0,0 +1,7 @@
+Just like most modern languages, Kotlin supports single-line (or end-of-line) and multi-line (block) comments.
+
+//This is an end-of-line comment
+
+
+/*This is a block comment
+   on multiple lines. */
