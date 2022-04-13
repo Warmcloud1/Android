deab95aa06e478f436ad25bdc7880b4ead8b4670
Warmcloud1 <52983230+Warmcloud1@users.noreply.github.com>
2022-04-13 14:17:27 +0800

    Create 2_3ForLoop.kt

---
+++ Kotlin/2_3ForLoop.kt
@@ -0,0 +1,26 @@
+
+// for loop
+val items = listOf("apple", "banana", "kiwifruit")
+for (item in items) {
+    println(item)
+}
+/*
+apple
+banana
+kiwifruit
+*/
+
+// for loop index
+val items = listOf("apple", "banana", "kiwifruit")
+for (index in items.indices) {
+    println("item at $index is ${items[index]}")
+}
+/*
+item at 0 is apple
+item at 1 is banana
+item at 2 is kiwifruit
+*/
+
+
+
+
